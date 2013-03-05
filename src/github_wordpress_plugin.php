8b4290bada2f1f4edfc4e988d0cb112893d80ae5
Clay Carpenter <claycarpenter@gmail.com>
2013-03-04 23:06:52 -0500

    Moves debug logger to its own file.

--- src/github_wordpress_plugin.php
+++ src/github_wordpress_plugin.php
@@ -7,20 +7,7 @@ Version: 0.0.1a
 Author: Clay Carpenter
 */
 
-/*
- * Simple error/debug logging utility. 
- */
-class Logger {
-	private static $log_file_location = 'debug.log';
-	
-	public static function log_debug($message) {
-		error_log(self::get_timestamp() . " - " . $message . "\n", 3, self::$log_file_location);
-	}
-	
-	private static function get_timestamp() {
-		return date(DateTime::RFC822);
-	}
-}
+require_once('logger.php');
 
 
 // TODO: Check for WP_http availability
