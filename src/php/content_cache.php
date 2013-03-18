f611e68d0e012fc088d05caf32e59c1caa9527b7
Clay Carpenter <claycarpenter@gmail.com>
2013-03-18 12:40:12 -0400

    Adds cache status option support.
    Content retrieval now honors the cache status option configuration.

--- src/php/content_cache.php
+++ src/php/content_cache.php
@@ -42,40 +42,76 @@ class ContentCache {
 	 * @return string					Raw content targeted by the provided content attributes.
 	 */
 	public static function get_content( $user_id, $repo_id, $content_path ) {
-		$cache_key = self::get_cache_key( $user_id, $repo_id, $content_path );
-		Logger::log_debug( "Looking for cached content under key: {$cache_key}" );
+		// Check to see if the cache is enabled.
+		if ( self::is_cache_enabled( ) ) {
+			$cache_key = self::get_cache_key( $user_id, $repo_id, $content_path );
+			Logger::log_debug( "Looking for cached content under key: {$cache_key}" );
 
-		$cache_content = get_transient( $cache_key );
+			$content = get_transient( $cache_key );
 
-		if ( false === $cache_content ) {
-			// Construct the URL to the GitHub content.
-			$content_url = "https://api.github.com/repos/{$user_id}/{$repo_id}/contents/{$content_path}";
+			if ( false === $content ) {
+				$content = self::get_remote_content( $user_id, $repo_id, $content_path );
 
-			Logger::log_debug( "Retrieving URL: {$content_url}" );
-
-			// Pull the content down.
-			$content_request_result = wp_remote_get( $content_url );
-
-			// The GitHub APIs will return a JSON-formatted response. Parse the body to find
-			// the file content.
-			$json_result = json_decode( $content_request_result[ 'body' ] );
-
-			$cache_content = $json_result -> content;
-
-			// The file content is transfered as base64-encoded data.
-			// Sanitize the content before being persisted to the cache..
-			$cache_content = sanitize_text_field( $cache_content );
-
-			// Push content into the cache.
-			// Set the default cache expire lifetime to one hour (60 * 60s).
-			set_transient( $cache_key, $cache_content, 60 * 60 );
+				// Push content into the cache.
+				// Set the default cache expire lifetime to one hour (60 * 60s).
+				set_transient( $cache_key, $content, 60 * 60 );
+			}
+		} else {
+			// Cache is not enabled. Pull down the remote content and return it directly.
+			$content = self::get_remote_content( $user_id, $repo_id, $content_path );
 		}
 
 		// All content in the cache is stored as sanitized base64-encoded data.
 		// Decode the data in order to get the raw content for embedding.
-		$content = base64_decode( $cache_content );
+		$content = base64_decode( $content );
 
 		return $content;
+	}
+
+	/**
+	 * Get the content specified by the given content attributes from the remote
+	 * GitHub repo.
+	 *
+	 * @param 	string	$user_id		The id of the GitHub user who owns the repository.
+	 *
+	 * @param	string	$repo_id		The id of the GitHub repository.
+	 *
+	 * @param	string	$content_path	The path (in GitHub) to the content. This is
+	 * relative to the GitHub repo root.
+	 *
+	 * @return string					Base64-encoded content as held in the remote GitHub
+	 * repository.
+	 */
+	private static function get_remote_content( $user_id, $repo_id, $content_path ) {
+		// Construct the URL to the GitHub content.
+		$content_url = "https://api.github.com/repos/{$user_id}/{$repo_id}/contents/{$content_path}";
+
+		Logger::log_debug( "Retrieving URL: {$content_url}" );
+
+		// Pull the content down.
+		$content_request_result = wp_remote_get( $content_url );
+
+		// The GitHub APIs will return a JSON-formatted response. Parse the body to find
+		// the file content.
+		$json_result = json_decode( $content_request_result[ 'body' ] );
+
+		$remote_content = $json_result -> content;
+
+		// The file content is transfered as base64-encoded data.
+		// Sanitize the content before being persisted to the cache..
+		$remote_content = sanitize_text_field( $remote_content );
+
+		return $remote_content;
+	}
+
+	/**
+	 * Checks to see if the cache status option is configure to enable or disable the
+	 * cache.
+	 *
+	 * @return	boolean				TRUE if the cache is enabled, FALSE otherwise.
+	 */
+	private static function is_cache_enabled( ) {
+		return OptionsManager::get_option_value( OptionsPageConstants::$SETTING_CACHE_STATUS ) === OptionsPageConstants::$SETTING_CACHE_STATUS_VALUE_ENABLED;
 	}
 
 	/**
